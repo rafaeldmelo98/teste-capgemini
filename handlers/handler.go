4c6fbd370897f75a0f615bce1c62d884bb9d7734
Rafael de Melo <rafael5665@hotmail.com>
2022-10-21 21:09:10 -0300

    Definindo rotas

---
+++ handlers/handler.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func CheckSequence(c echo.Context) error {
+	return nil
+}
+
+func Stats(c echo.Context) error {
+	return nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"teste-capgemini/handlers"
 
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/stats", handlers.Stats)
+	e.POST("/sequence", handlers.CheckSequence)
+	e.Logger.Fatal(e.Start(":8080"))
 }
