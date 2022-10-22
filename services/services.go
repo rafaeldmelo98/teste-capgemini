de2dd1c2e5cb46d32bc0f9037b5b50e769cdde28
Rafael de Melo <rafael5665@hotmail.com>
2022-10-22 20:29:49 -0300

    Função para checar sequencia

--- services/services.go
+++ services/services.go
@@ -16,3 +16,80 @@ func MapSequence(letter string, sequenceList []string) [][]bool {
 
 	return matrix
 }
+
+func FindValidSequence(matrix [][]bool) int {
+	sequencesFounded := 0
+	for indexRow, row := range matrix {
+		for indexColumn := range row {
+			if CheckForDiagonalSequence(matrix, indexRow, indexColumn) {
+				sequencesFounded++
+				continue
+			}
+			if CheckForLineSequence(matrix, indexRow, indexColumn) {
+				sequencesFounded++
+				continue
+			}
+			if CheckForColumnSequence(matrix, indexRow, indexColumn) {
+				sequencesFounded++
+				continue
+			}
+		}
+	}
+
+	return sequencesFounded
+}
+
+func CheckForDiagonalSequence(matrix [][]bool, actualRow, actualColumn int) bool {
+	countLetter := 0
+	maxRows, maxColumns := GetMatrixSize(matrix)
+	for row := actualRow; row < maxRows; row++ {
+		for column := actualColumn; column < maxColumns; column++ {
+			if matrix[row][column] {
+				countLetter++
+			}
+			if countLetter == 4 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func CheckForLineSequence(matrix [][]bool, actualRow, actualColumn int) bool {
+	countLetter := 0
+	_, maxColumns := GetMatrixSize(matrix)
+	for column := actualColumn; column < maxColumns; column++ {
+		if matrix[actualRow][column] {
+			countLetter++
+		}
+		if countLetter == 4 {
+			return true
+		}
+	}
+	return false
+}
+
+func CheckForColumnSequence(matrix [][]bool, actualRow, actualColumn int) bool {
+	countLetter := 0
+	maxRows, _ := GetMatrixSize(matrix)
+	for row := actualRow; row < maxRows; actualRow++ {
+		if matrix[row][actualColumn] {
+			countLetter++
+		}
+		if countLetter == 4 {
+			return true
+		}
+	}
+	return false
+}
+
+func GetMatrixSize(matrix [][]bool) (int, int) {
+	maxRows := len(matrix)
+	var maxColumns int
+	if maxRows > 0 {
+		maxColumns = len(matrix[0])
+	} else {
+		maxColumns = 0
+	}
+	return maxRows, maxColumns
+}
